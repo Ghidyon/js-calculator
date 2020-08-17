43385a8628a01cfd4e972366c28465a917383252
Ghidyon <akunanagideon@gmail.com>
2020-08-17 16:26:51 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,8 @@
+alert('Addition Calculator');
+var x = prompt('Enter your favorite number');
+var y = prompt('Enter your age');
+
+var sum = Number(x) + Number(y);
+console.log(sum);
+alert('The sum of both numbers is: ' + sum);
+document.write('The sum of both numbers is: ' + sum);
